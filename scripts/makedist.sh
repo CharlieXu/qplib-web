57ccdd7b4f5c39a0c7732718d7e64fba34463cc0
Stefan Vigerske <svigerske@gams.com>
2016-12-28 15:53:30 +0100

    add script to zip up qplib

---
+++ scripts/makedist.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SCRIPTDIR=`dirname $0`
+SCRIPTDIR=`cd $SCRIPTDIR ; pwd`
+
+cd $SCRIPTDIR/..
+
+rm -f qplib.zip
+ln -s . qplib
+
+zip -r qplib.zip qplib/data/{meta,gms,lp} \
+   qplib/html \
+   qplib/scripts/{query.py,metadata.py}
+    
+rm qplib
